89996066a241038d1d26f5d09514e38663580f1d
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-04-08 00:33:08 +0800

    progressing

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,40 @@
 const express = require("express");
+const dotenv = require('dotenv')
+const { getFirestore } = require("firebase/firestore");
+const { initializeApp } = require("firebase/app");
+const {
+  collection,
+  setDoc,
+  doc,
+  onSnapshot,
+  addDoc,
+  query,
+  orderBy,} = require("firebase/firestore");
 const app = express();
 const port = 8080;
 app.use(express.json());
-
-// GET - Get / List
-// POST - Create
-// PUT - Replace / edit (update)
-// PATCH - Edit (update)
-// DELETE - Delete
-
-// 100-199 -> Inforational Responses
-// 200-299 -> Success
-// 300-399 -> Redirect
-// 400-499 -> Client Error
-// 500-599 -> Server Error
-
-// JSON -> JavaScript Object Notation
+dotenv.config();
+const {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID
+} = process.env
+  firebaseConfig = initializeApp({
+        apiKey: API_KEY,
+        authDomain: AUTH_DOMAIN,
+        projectId: PROJECT_ID,
+        storageBucket: STORAGE_BUCKET,
+        messagingSenderId: MESSAGING_SENDER_ID,
+        appId: APP_ID
+      })
+const db = getFirestore()
 let data = [
   {
     id: 1,
+    api: API_KEY,
     title: "Fits 15 Laptops",
     price: 109.95,
     description: "Stash your laptop",
@@ -29,7 +45,7 @@ let data = [
       count: 120,
     },
   },
-];
+]
 app.post("/login", (req, res) => {
   console.log(req.query);
   console.log(req.body);
@@ -42,14 +58,31 @@ app.post("/logout", (req, res) => {
   res.send("OK");
 });
 app.get("/products", (req, res) => {
-  res.send(data);
-});
-app.get("/products", async (req, res) => {
   console.log(req.query);
   console.log(req.body);
-  await db.collection().doc().set();
-  
-  res.send("success");
+  const data = req.body;
+  res.send(req.body);
+});
+app.post("/products", async(req, res) => {
+  console.log(req.query);
+  console.log(req.body);
+  // const data = req.body
+  req.body =   {
+    id: 1,
+     api: API_KEY,
+     title: "Fits 15 Laptops",
+     price: 109.95,
+     description: "Stash your laptop",
+     category: "men's clothing",
+     image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+     rating: {
+       rate: 3.9,
+       count: 120,
+     },
+}
+  const citiesRef = collection(db, "datas");
+  await setDoc(doc(citiesRef, '2ysqgMf0p8qurWHoof9o'),req.body)
+  res.send(req.body);
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
