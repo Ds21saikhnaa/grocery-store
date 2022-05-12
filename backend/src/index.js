eba2719eaa7dd59848a0d32271a62b782b7b43cc
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-05-13 00:33:36 +0800

    dasdas

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,22 +23,13 @@ app.use(express.urlencoded({limit: '50mb'}));
 // app.use(express.limit(100000000));
 app.use(cors());
 dotenv.config();
-const {
-  API_KEY,
-  AUTH_DOMAIN,
-  PROJECT_ID,
-  STORAGE_BUCKET,
-  MESSAGING_SENDER_ID,
-  APP_ID,
-} = process.env;
-console.log(API_KEY);
 const firebaseConfig = initializeApp({
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 });
 const db = getFirestore();
 const isAuthenticated = async (req, res, next) => {
